5c2f26ab2011fce2ebb612c6baa5725a0fe21ad1
multisensorylb <43795925+multisensorylb@users.noreply.github.com>
2019-11-18 23:10:34 -0500

    Read xpt files through R/Python/SAS

---
+++ Codes/R/importXPTfiles.R
@@ -0,0 +1,13 @@
+library(SASxport)
+
+demographics = read.xport("C:/Users/gkarthik/Desktop/SASUniversityEdition/myfolders/home/new/stats506/Demographics_2015_16.XPT")
+BMI = read.xport("C:/Users/gkarthik/Desktop/SASUniversityEdition/myfolders/home/new/stats506/Body_measures_2015_16.XPT")
+blood_pressure = read.xport("C:/Users/gkarthik/Desktop/SASUniversityEdition/myfolders/home/new/stats506/Blood_Pressure_2015_16.XPT")
+food_1day = read.xport("C:/Users/gkarthik/Desktop/SASUniversityEdition/myfolders/home/new/stats506/Dietary_foods_firstday_2015_16.XPT")
+food_2day = read.xport("C:/Users/gkarthik/Desktop/SASUniversityEdition/myfolders/home/new/stats506/Dietary_foods_secondday_2015_16.XPT")
+nutrients_1day = read.xport("C:/Users/gkarthik/Desktop/SASUniversityEdition/myfolders/home/new/stats506/Dietary_nutrients_firstday_2015_16.XPT")
+nutrients_2day = read.xport("C:/Users/gkarthik/Desktop/SASUniversityEdition/myfolders/home/new/stats506/Dietary_nutrients_secondday_2015_16.XPT")
+supplements_1day = read.xport("C:/Users/gkarthik/Desktop/SASUniversityEdition/myfolders/home/new/stats506/Dietary_supplements_firstday_2015_16.XPT")
+supplements_2day = read.xport("C:/Users/gkarthik/Desktop/SASUniversityEdition/myfolders/home/new/stats506/Dietary_supplements_secondday_2015_16.XPT")
+
+
