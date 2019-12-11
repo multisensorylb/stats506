1d30f62c4ca5931b8c9b28475cc695ec03faebc5
multisensorylb <43795925+multisensorylb@users.noreply.github.com>
2019-12-11 18:59:36 -0500

    Update importXPTfiles.py

--- Codes/Python/importXPTfiles.py
+++ Codes/Python/importXPTfiles.py
@@ -7,14 +7,14 @@ Created on Mon Nov 18 23:05:42 2019
 
 import pandas as pd
 
-demographics = pd.read_sas("C:\\Users\\gkarthik\\Desktop\\SASUniversityEdition\\myfolders\\home\\new\\stats506\\Demographics_2015_16.XPT")
-BMI = pd.read_sas("C:\\Users\\gkarthik\\Desktop\\SASUniversityEdition\\myfolders\\home\\new\\stats506\\Body_measures_2015_16.XPT")
-blood_pressure = pd.read_sas("C:\\Users\\gkarthik\\Desktop\\SASUniversityEdition\\myfolders\\home\\new\\stats506\\Blood_Pressure_2015_16.XPT")
-food_1day = pd.read_sas("C:\\Users\\gkarthik\\Desktop\\SASUniversityEdition\\myfolders\\home\\new\\stats506\\Dietary_foods_firstday_2015_16.XPT")
-food_2day = pd.read_sas("C:\\Users\\gkarthik\\Desktop\\SASUniversityEdition\\myfolders\\home\\new\\stats506\\Dietary_foods_secondday_2015_16.XPT")
-nutrients_1day = pd.read_sas("C:\\Users\\gkarthik\\Desktop\\SASUniversityEdition\\myfolders\\home\\new\\stats506\\Dietary_nutrients_firstday_2015_16.XPT")
-nutrients_2day = pd.read_sas("C:\\Users\\gkarthik\\Desktop\\SASUniversityEdition\\myfolders\\home\\new\\stats506\\Dietary_nutrients_secondday_2015_16.XPT")
-supplements_1day = pd.read_sas("C:\\Users\\gkarthik\\Desktop\\SASUniversityEdition\\myfolders\\home\\new\\stats506\\Dietary_supplements_firstday_2015_16.XPT")
-supplements_2day = pd.read_sas("C:\\Users\\gkarthik\\Desktop\\SASUniversityEdition\\myfolders\\home\\new\\stats506\\Dietary_supplements_secondday_2015_16.XPT")
+demographics = pd.read_sas(".\Demographics_2015_16.XPT")
+BMI = pd.read_sas(".\Body_measures_2015_16.XPT")
+blood_pressure = pd.read_sas(".\Blood_Pressure_2015_16.XPT")
+food_1day = pd.read_sas(".\Dietary_foods_firstday_2015_16.XPT")
+food_2day = pd.read_sas(".\Dietary_foods_secondday_2015_16.XPT")
+nutrients_1day = pd.read_sas(".\Dietary_nutrients_firstday_2015_16.XPT")
+nutrients_2day = pd.read_sas(".\Dietary_nutrients_secondday_2015_16.XPT")
+supplements_1day = pd.read_sas(".\stats506\\Dietary_supplements_firstday_2015_16.XPT")
+supplements_2day = pd.read_sas(".\stats506\\Dietary_supplements_secondday_2015_16.XPT")
 
 
